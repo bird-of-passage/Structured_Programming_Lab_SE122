9729b2a0d082bc33e7f694aad126499ce4f58cde
Invincible-21 <shantobd1969@gmail.com>
2022-05-23 10:08:22 +0600

    contional class

---
+++ Condinational_operators/Lab_if_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    if (a > b)
+    {
+        printf("A is bigger");
+    }
+    else
+    {
+        printf("B is bigger");
+    }
+
+    return 0;
+}
---
+++ Condinational_operators/allow_or_not.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float gpa = 4.00;
+    int age = 18;
+
+    if (gpa == 4.00 || age == 18)
+    {
+        printf("You are allowed");
+    }
+    else
+    {
+        printf("You are not allowed");
+    }
+
+    return 0;
+}
