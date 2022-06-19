a63a433980eb39841a9a2b4f55debbaaae232813
Invincible-21 <shantobd1969@gmail.com>
2022-06-20 01:05:51 +0600

    files updated

---
+++ Classes/Array class/ex_01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={2,4,5,7,1}; //aitai array
+
+	int i; //eta nici array index er jonno
+
+
+	for(i=0;i<5;i++){
+
+		printf("%d\n",a[i]);
+
+	}
+
+	return 0;
+
+}
---
+++ Classes/Array class/sample gui.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int main()
+{
+    MessageBoxA( NULL, "Hello world !", "Hello", MB_OK );
+}
